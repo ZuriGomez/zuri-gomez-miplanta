dc16464882b08958526358c36e1354447f27ed2d
Zuri Gomez <zgo522@gmail.com>
2024-11-25 15:29:37 -0500

    Add logo and styles to SignUpPage

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import Logo_signup from "../../assets/logos/miplanta_logo2.svg";
+import "../SignUpPage/SignUpPage.scss"
 
 function SignUpPage() {
   return (
-    <>
-      <SignUpForm/>
-    </>
+    <div className="signup__page">
+      <img
+        className="signup__page-logo"
+        src={Logo_signup}
+        alt="Logo for sign up page"
+      />
+      <SignUpForm />
+    </div>
   );
 }
 
