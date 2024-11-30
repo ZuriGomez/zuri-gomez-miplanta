938eb3e4f5246963571a3dd3a9fae9e011482fd1
Zuri Gomez <zgo522@gmail.com>
2024-11-30 09:56:22 -0500

    Add a count of the user lisitngs to be in  the dashboard

--- src/components/UserListingList/UserListingList.jsx
+++ src/components/UserListingList/UserListingList.jsx
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-// import SnakePlant1 from "../../assets/images/snake_plant1.jpg";
-// import SnakePlant2 from "../../assets/images/snake-plant2.jpg";
-// import Monstera from "../../assets/images/monstera.jpg";
-// import GoldenPothos from "../../assets/images/golden-pothos.jpg";
 import editIcon from "../../assets/icons/edit.svg";
 import "../UserListingList/UserListingList.scss";
 
@@ -53,7 +49,7 @@ function UserListingList() {
           <div className="user listings-list__item-wrapper">
             <img
               className="user listings-list__item-wrapper-img"
-              src={`${BASE_URL}/public/uploads/${listing.photo}`}
+              src={`${BASE_URL}/${listing.photo}`}
               alt={listing.title}
             />
           </div>
@@ -77,82 +73,5 @@ function UserListingList() {
     </div>
   );
 }
-//     <div className="user listings-list">
-//       <h3 className="user listings-list__list-title">my listings</h3>
-//       <Link to="/api/listings/7" className="user listings-list__item">
-//         <div className="user listings-list__item-wrapper">
-//           <img
-//             className="user listings-list__item-wrapper-img"
-//             src={SnakePlant1}
-//             alt="Snake Plant"
-//           />
-//         </div>
-//         <div className="user listings-list__item-details">
-//           <div className="user listings-list__item-details__top">
-//             <p className="user listings-list__item-details__top__title">
-//               Beautiful Snake Plant - pot included
-//             </p>
-//             <img className="user listings-list__item-details__top__edit-icon" src={editIcon} alt="Edit Icon" />
-//           </div>
-//           <p className="user listings-list__item-details__price">$40</p>
-//         </div>
-//       </Link>
-//       <Link to="/api/listings/3" className="user listings-list__item">
-//         <div className="user listings-list__item-wrapper">
-//           <img
-//             className="user listings-list__item-wrapper-img"
-//             src={Monstera}
-//             alt="Monstera"
-//           />
-//         </div>
-//         <div className="user listings-list__item-details">
-//         <div className="user listings-list__item-details__top">
-//             <p className="user listings-list__item-details__top__title">
-//               Voluminous Monstera
-//             </p>
-//             <img className="user listings-list__item-details__top__edit-icon" src={editIcon} alt="Edit Icon" />
-//           </div>
-//           <p className="user listings-list__item-details__price">$60</p>
-//         </div>
-//       </Link>
-//       <Link to="/api/listings/6" className="user listings-list__item">
-//         <div className="user listings-list__item-wrapper">
-//           <img
-//             className="user listings-list__item-wrapper-img"
-//             src={SnakePlant2}
-//             alt="SnakePlant2"
-//           />
-//         </div>
-//         <div className="user listings-list__item-details">
-//         <div className="user listings-list__item-details__top">
-//             <p className="user listings-list__item-details__top__title">
-//               Healthy Snake Plant
-//             </p>
-//             <img className="user listings-list__item-details__top__edit-icon" src={editIcon} alt="Edit Icon" />
-//           </div>
-//           <p className="user listings-list__item-details__price">$20</p>
-//         </div>
-//       </Link>
-//       <Link to="/api/listings/9" className="user listings-list__item">
-//         <div className="user listings-list__item-wrapper">
-//           <img
-//             className="user listings-list__item-wrapper-img"
-//             src={GoldenPothos}
-//             alt="Golden Pothos"
-//           />
-//         </div>
-//         <div className="user listings-list__item-details">
-//         <div className="user listings-list__item-details__top">
-//             <p className="user listings-list__item-details__top__title">
-//               Golden Pothos
-//             </p>
-//             <img className="user listings-list__item-details__top__edit-icon" src={editIcon} alt="Edit Icon" />
-//           </div>
-//           <p className="user listings-list__item-details__price">$45</p>
-//         </div>
-//       </Link>
-//     </div>
-//   );
-// }
 
 export default UserListingList;
--- src/components/UserProfileDashboard/UserProfileDashboard.jsx
+++ src/components/UserProfileDashboard/UserProfileDashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import UserAvatar from "../../assets/images/avatar-placeholder.png";
 import MainButton from "../Buttons/MainButton/MainButton";
 import listingsIcon from "../../assets/icons/listings.svg";
@@ -6,10 +6,43 @@ import favoriteIcon from "../../assets/icons/favorites.svg";
 import purchasesIcon from "../../assets/icons/purchased.svg";
 import "../UserProfileDashboard/UserProfileDashboard.scss";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 function UserProfileDashboard () {
+
+  const [dashboardData, setDashboardData] = useState({
+    listings: 0,
+    favorites: 0, // Update with backend logic when needed
+    purchased: 0, // Update with backend logic when needed
+  });
+
   const navigate = useNavigate();
   
+  useEffect(() => {
+    const fetchUserListings = async () => {
+      try {
+        const token = localStorage.getItem("jwtToken");
+        const response = await axios.get(`${BASE_URL}/api/listings/user-listings`, 
+          {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const listingsCount = response.data.length;
+        setDashboardData((prevData) => ({
+          ...prevData,
+          listings: listingsCount,
+        }));
+      } catch (error) {
+        console.error("Error fetching user listings:", error);
+      }
+    };
+
+    fetchUserListings();
+  }, []);
+
   const handleCreateListingClick = () => {
     navigate("/add-listing");
   };
@@ -25,17 +58,17 @@ function UserProfileDashboard () {
         <div className="user-profile__dashboard-item">
           <img className="user-profile__dashboard-item-icon" src={listingsIcon} alt="Listing Icon" />
           <p className="user-profile__dashboard-item-label">listings</p>
-          <p className="user-profile__dashboard-item-count">4</p>
+          <p className="user-profile__dashboard-item-count">{dashboardData.listings}</p>
         </div>
         <div className="user-profile__dashboard-item">
           <img className="user-profile__dashboard-item-icon" src={favoriteIcon} alt="Favorites" />
           <p className="user-profile__dashboard-item-label">favorites</p>
-          <p className="user-profile__dashboard-item-count">3</p>
+          <p className="user-profile__dashboard-item-count">{dashboardData.favorites}</p>
         </div>
         <div className="user-profile__dashboard-item">
           <img className="user-profile__dashboard-item-icon" src={purchasesIcon} alt="Purchases" />
           <p className="user-profile__dashboard-item-label">purchased</p>
-          <p className="user-profile__dashboard-item-count">2</p>
+          <p className="user-profile__dashboard-item-count">{dashboardData.purchased}</p>
         </div>
       </div>
     </div>
