f62e76f33e099b69b539d951bde0d71ccb06089e
Zuri Gomez <zgo522@gmail.com>
2024-11-30 23:18:02 -0500

    Add styles to the review list for mobile screen.

--- src/components/ReviewsList/ReviewsList.jsx
+++ src/components/ReviewsList/ReviewsList.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import "../ReviewsList/ReviewsList.scss";
+import avatar from "../../assets/images/avatar-placeholder.png";
 
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 function ReviewsList() {
-
   const [reviews, setReviews] = useState([]);
   const [error, setError] = useState(null);
   const { sellerId } = useParams();
@@ -17,17 +18,19 @@ function ReviewsList() {
         console.log(response.data);
 
         if (Array.isArray(response.data)) {
-            setReviews(response.data);
-          } else {
-            throw new Error("Invalid data format: Reviews data should be an array.");
-          }
-        } catch (err) {
-          setError(err.message);
-          console.error("Error fetching reviews:", err);
+          setReviews(response.data);
+        } else {
+          throw new Error(
+            "Invalid data format: Reviews data should be an array."
+          );
         }
-      };
-    if (sellerId){
-        fetchReviews();
+      } catch (err) {
+        setError(err.message);
+        console.error("Error fetching reviews:", err);
+      }
+    };
+    if (sellerId) {
+      fetchReviews();
     }
   }, [sellerId]);
 
@@ -41,16 +44,27 @@ function ReviewsList() {
 
   return (
     <div className="reviews-list">
-        <h3>{reviews.length} reviews</h3>
-        {reviews.map((review) => (
-          <div key={review.id} className="review">
-            <h3>{review.reviewer_name}</h3>
-            <p>{review.review_text}</p>
-            <span>{new Date(review.created_at).toLocaleDateString()}</span>
+      <h3 className="reviews-list__title">{reviews.length} reviews</h3>
+      {reviews.map((review) => (
+        <div key={review.id} className="reviews-list__review">
+          <img className="reviews-list__review-avatar" src={avatar} alt="Reviewer Avatar" />
+          <div className="reviews-list__review-container">
+            <div className="reviews-list__review-container-wrapper">
+              <h3 className="reviews-list__review-container-wrapper__name">
+                {review.reviewer_name}
+              </h3>
+              <span className="reviews-list__review-cotnainer-wrapper__date">
+                {new Date(review.created_at).toLocaleDateString()}
+              </span>
+            </div>
+            <p className="reviews-list__review-container-review">
+              {review.review_text}
+            </p>
           </div>
-        ))}
+        </div>
+      ))}
     </div>
   );
-};
+}
 
 export default ReviewsList;
