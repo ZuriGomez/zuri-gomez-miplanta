2f8e68c682a70c6558d693eacacc1b702337508b
Zuri Gomez <zgo522@gmail.com>
2024-11-30 23:20:39 -0500

    Add a header in the sellerprofilepage.

--- src/pages/SellerProfilePage/SellerProfilePage.jsx
+++ src/pages/SellerProfilePage/SellerProfilePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReviewsList from "../../components/ReviewsList/ReviewsList";
 import SellerProfile from "../../components/SellerProfile/SellerProfile";
+import Header from "../../components/Header/Header";
 
 function SellerProfilePage() {
   return (
   <>
-  <SellerProfile/>
-  <ReviewsList/> 
+    <Header/>
+    <SellerProfile/>
+    <ReviewsList/> 
   </>
   )
 }
